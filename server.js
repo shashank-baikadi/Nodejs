96ab94ede1e141588fe909a5c12c830a9098ec7f
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-04-30 17:41:08 +0530

    router

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express =require('express');
+const app = express();
+const router = require('./router/auth-router');
+
+app.use('/api/auth', router);
+
+app.get('/', (req, res) => {
+    res.send('Hello World..')
+})
+
+app.listen(3000);
+
+
